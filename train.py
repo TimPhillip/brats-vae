26cfda424d17c357edcad7822f9e673b92315280
Tim Schneider <timphillip.schneider@t-online.de>
2024-12-04 14:44:52 +0100

    support GPU device

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def plot_vae_samples(samples):
         ax[0].imshow(img, cmap='Greys_r')
 
 
-def train(model, data, num_epochs=100):
+def train(model, data, device, num_epochs=100):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     data_loader = torch.utils.data.DataLoader(
@@ -40,6 +40,9 @@ def train(model, data, num_epochs=100):
 
         model.train()
         for batch in data_loader:
+
+            batch = batch.to(device)
+
             optimizer.zero_grad()
 
             elbo = model.elbo(batch)
@@ -54,14 +57,22 @@ def main():
     parser.add_argument("--dataset", type=str, required=True, help="Path to brain dataset.")
     parser.add_argument("--num_layers", type=int, default=7, help="Number of layers.")
     parser.add_argument("--latent_size", type=int, default=32, help="Size of latent space.")
+    parser.add_argument("--device", type=str, choices=['cpu', 'gpu'], default='gpu', help="Which device to use.")
     args = parser.parse_args()
+
+    if args.device == 'gpu' and torch.cuda.is_available() and torch.cuda.device_count() >= 1:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    print(f"Running experiment on {device}")
 
     dataset = BrainDataset(args.dataset)
     model = VAE(input_size=(240, 240),
                 latent_size=args.latent_size,
                 num_encoder_layers=args.num_layers,
                 num_decoder_layers=args.num_layers)
-    train(model, dataset, num_epochs=args.epochs)
+    model.to(device)
+    train(model, dataset, device, num_epochs=args.epochs)
 
 
 if __name__ == '__main__':
