3ecec75102f8a9d503c02df3219145299b52d3a3
Tim Schneider <timphillip.schneider@t-online.de>
2024-12-04 15:01:40 +0100

    added more options to the config

--- train.py
+++ train.py
@@ -21,22 +21,26 @@ def plot_vae_samples(samples):
         ax[0].imshow(img, cmap='Greys_r')
 
 
-def train(model, data, device, num_epochs=100):
+def train(model, data, device,
+          num_epochs=100,
+          batch_size=64,
+          plot_samples=True):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     data_loader = torch.utils.data.DataLoader(
         dataset=data,
-        batch_size=64,
-        shuffle=True,
+        batch_size=batch_size,
+        shuffle=False
     )
 
     for epoch in range(num_epochs):
 
-        with torch.no_grad():
-            model.eval()
-            samples = model.sample(num_samples=9)
-            plot_vae_samples(samples)
-            plt.savefig(f'samples/samples_epoch_{epoch}.png')
+        if plot_samples:
+            with torch.no_grad():
+                model.eval()
+                samples = model.sample(num_samples=9)
+                plot_vae_samples(samples)
+                plt.savefig(f'samples/samples_epoch_{epoch}.png')
 
         model.train()
         for batch in data_loader:
@@ -59,6 +63,8 @@ def main():
     parser.add_argument("--num_layers", type=int, default=7, help="Number of layers.")
     parser.add_argument("--latent_size", type=int, default=32, help="Size of latent space.")
     parser.add_argument("--device", type=str, choices=['cpu', 'gpu'], default='gpu', help="Which device to use.")
+    parser.add_argument("--plot_samples", type=bool, default=True, help="Whether to plot samples.")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size.")
     args = parser.parse_args()
 
     if args.device == 'gpu' and torch.cuda.is_available() and torch.cuda.device_count() >= 1:
@@ -74,7 +80,10 @@ def main():
                 num_encoder_layers=args.num_layers,
                 num_decoder_layers=args.num_layers)
     model.to(device)
-    train(model, dataset, device, num_epochs=args.epochs)
+    train(model, dataset, device,
+          num_epochs=args.epochs,
+          plot_samples=args.plot_samples,
+          batch_size=args.batch_size)
 
 
 if __name__ == '__main__':
